0d9a98807f4894ab9be60d18b83c9c3457da49cf
Hue <clementhue@hotmail.fr>
2020-02-18 11:21:55 +0100

    Pointeur de fonction tp3

--- C/TP2/quicksort.c
+++ C/TP2/quicksort.c
@@ -8,10 +8,13 @@
 
 int main(){
     int tab[TAILLE] ;
+    enum Tri tri;
     init_tableau(tab);
     printf("avant\n");
     afficher_tableau(tab);
-    sort(tab);
+    printf("Entrer 0 pour croissant 1 pour décroissant 2 pour paire\n");
+    scanf("%d",&tri);
+    sort(tab,tri);
     printf("après\n");
     afficher_tableau(tab);
     if (test_tri(tab)){
@@ -30,6 +33,7 @@ void afficher_tableau(int tab[]){
     printf("\n");
 }
 
+
 void init_tableau(int *tab){
     srand(time(NULL));
     for (int i = 0;i<TAILLE;i++){
@@ -46,25 +50,26 @@ bool test_tri(int *tab){
     return true;
 }
 
-void sort(int*tab){
-    quickSort(tab,0,TAILLE-1);
+void sort(int*tab, enum Tri tri){
+    bool (*comp)(int,int,enum Tri) = comparaison;
+    quickSort(tab,tri,0,TAILLE-1,comp);
 }
 
-void quickSort(int *tab, int L,int R){
+void quickSort(int *tab, enum Tri tri, int L,int R, bool (*comp)(int,int,enum Tri)){
     if (L>=R)
         return;
-    int pivot = partition(tab,L,R);
-    quickSort(tab,L,pivot-1); 
-    quickSort(tab,pivot+1,R);
+    int pivot = partition(tab,tri,L,R,comp);
+    quickSort(tab,tri,L,pivot-1,comp); 
+    quickSort(tab,tri,pivot+1,R,comp);
 }
 
-int partition(int *tab,int L,int R){
+int partition(int *tab, enum Tri tri ,int L,int R, bool (*comp)(int,int,enum Tri)){
     int pivot = tab[L];
     int index_pivot = L;
     int temp = 0;
     L++;
     while  (L<=R){
-        if (tab[L] <= pivot)
+        if ((*comp)(tab[L],pivot,tri))
             L++;
         else{
             temp = tab[L];
@@ -77,4 +82,30 @@ int partition(int *tab,int L,int R){
     tab[index_pivot] = tab[R];
     tab[R] = temp;
     return R;
+}
+
+bool comparaison(int elem,int pivot, enum Tri tri){
+    switch(tri){
+        case CROISSANT:
+            if (elem <= pivot)
+                return true;
+            else
+                return false;
+            break;
+        case DECROISSANT:
+            if (elem >= pivot)
+                return true;
+            else 
+                return false;
+            break;
+        case PAIRE:
+            if (elem %2 == pivot %2)  
+                return elem <= pivot;
+            else if (elem%2 == 0)
+                return true;
+            else 
+                return false;
+            break; 
+        default: break;
+    }
 }
--- C/TP2/quicksort.h
+++ C/TP2/quicksort.h
@@ -3,12 +3,20 @@
 #include <stdbool.h>
 
 #define TAILLE 20
+
+enum Tri {
+    CROISSANT,
+    DECROISSANT,
+    PAIRE,
+    IMPAIRE
+};
+
 void afficher_tableau(int[TAILLE]);
 void init_tableau(int*);
 bool test_tri(int*);
-void quickSort(int*,int,int);
-void sort(int*);
-int partition(int*,int,int);
-
+void quickSort(int *, enum Tri , int ,int , bool (*)(int,int,enum Tri));
+void sort(int*, enum Tri);
+int partition(int* , enum Tri ,int ,int , bool (*)(int,int,enum Tri));
+bool comparaison(int,int,enum Tri);
 
 #endif
